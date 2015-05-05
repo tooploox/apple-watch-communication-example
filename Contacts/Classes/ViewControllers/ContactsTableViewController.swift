d99753763e71c4bdc2e081c8cd34a378d8dddff8
Mateusz Nadolski <mateusz.nadolski@tooploox.com>
2015-05-05 19:12:43 +0200

    Add ContactsTableViewController

---
+++ Contacts/Classes/ViewControllers/ContactsTableViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ContactsTableViewController.swift
+//  Contacts
+//
+//  Created by Mateusz Nadolski on 05/05/15.
+//  Copyright (c) 2015 Tooploox. All rights reserved.
+//
+
+import UIKit
+
+class ContactsTableViewController: UITableViewController {    
+}
